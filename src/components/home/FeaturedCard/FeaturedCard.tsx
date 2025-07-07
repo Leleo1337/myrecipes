ae2d149b45e05e5c52bae73cae2c909c40297d5e
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-07 17:52:22 -0300

    feat: create featured card

---
+++ src/components/home/FeaturedCard/FeaturedCard.tsx
@@ -0,0 +1,42 @@
+import { Heart, Star, Users2 } from 'lucide-react';
+import FeaturedCardItem from './FeaturedCardItem';
+
+export default function FeaturedCard() {
+	return (
+		<div className='flex flex-col items-center justify-center lg:flex-row gap-4 mt-12'>
+			<FeaturedCardItem
+				icon={
+					<Users2
+						size={30}
+						color='white'
+					/>
+				}
+				title='Comunidade Ativa'
+				text='Conecte-se com outros amantes de culinária, compartilhe experiências e aprenda novas receitas.'
+				color='darkgreen'
+			/>
+			<FeaturedCardItem
+				icon={
+					<Heart
+						size={30}
+						color='white'
+					/>
+				}
+				title='Favoritos e Likes'
+				text='Salve receitas preferidas, deixe seu like e comente nas criações da comunidade gastronômica.'
+				color='green'
+			/>
+			<FeaturedCardItem
+				icon={
+					<Star
+						size={30}
+						color='white'
+					/>
+				}
+				title='Receitas Variadas'
+				text='Descubra pratos que vão dos mais casuais aos mais elaborados, para todos os níveis e ocasiões.'
+				color='orange'
+			/>
+		</div>
+	);
+}
---
+++ src/components/home/FeaturedCard/FeaturedCardItem.tsx
@@ -0,0 +1,40 @@
+type FeaturedCardProps = {
+	icon: any;
+	title: string;
+	text: string;
+	color: 'darkgreen' | 'green' | 'orange'
+};
+
+const colorClasses = {
+	darkgreen: {
+		bg: 'bg-emerald-500/10',
+		border: 'border-emerald-500/20',
+		iconBg: 'bg-emerald-600',
+	},
+	green: {
+		bg: 'bg-lime-500/10',
+		border: 'border-lime-500/20',
+		iconBg: 'bg-lime-600',
+	},
+	orange: {
+		bg: 'bg-orange-400/10',
+		border: 'border-orange-400/20',
+		iconBg: 'bg-orange-500',
+	},
+};
+
+export default function FeaturedCardItem({ icon, title, text, color }: FeaturedCardProps) {
+	const selectedColor = colorClasses[color];
+
+	return (
+		<div className={`${selectedColor.bg} ${selectedColor.border} w-full border p-4 rounded-xl py-8 lg:py-12 shadow-xs hover:shadow-xl transition ease-in duration-100`}>
+			<div className='flex flex-col items-center justify-center space-y-2'>
+				<div className={`flex items-center justify-center w-16 h-16 ${selectedColor.iconBg} rounded-2xl shadow-md`}>
+					{icon}
+				</div>
+				<p className='font-semibold text-gray-900 text-center text-xl'>{title}</p>
+			</div>
+			<p className='text-gray-600 text-center mt-2 lg:text-lg'>{text}</p>
+		</div>
+	);
+}
