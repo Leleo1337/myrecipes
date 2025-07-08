2dfc58ef746b8279d69b5177c5ac506aecef6d90
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-08 17:42:34 -0300

    feat: add header & sidebar

--- src/pages/recipes/Recipes.tsx
+++ src/pages/recipes/Recipes.tsx
@@ -1,7 +1,35 @@
-export default function Recipes(){
-    return(
-        <>
-        
-        </>
-    )
+import { useEffect, useState } from 'react';
+import SideBar from '../../components/recipes/sideBar';
+import Header from '../../components/recipes/Header';
+
+export default function Recipes() {
+	const [sidebarOpen, setSidebarOpen] = useState(true);
+
+	function handleSideBarToggle() {
+		setSidebarOpen(!sidebarOpen);
+	}
+
+	useEffect(() => {
+		document.body.style.backgroundColor = '#f3f4f6';
+		return () => {
+			document.body.style.backgroundColor = 'white';
+		};
+	}, []);
+
+	return (
+		<>
+			{sidebarOpen && <div className='absolute z-2 w-screen h-screen bg-black/70 md:hidden'></div>}
+			<main>
+				<header>
+					{/* Mobile Sidebar */}
+					<SideBar
+						toggle={handleSideBarToggle}
+						open={sidebarOpen}
+					/>
+					<Header toggleSide={handleSideBarToggle} />
+				</header>
+				<section></section>
+			</main>
+		</>
+	);
 }
