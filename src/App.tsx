433f7fb5db4eed08dfeb8f9fa97927c1f664665e
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-08 00:01:22 -0300

    feat: add some routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 
 //elements
-import Home from './pages/Home'
+import Home from './pages/Home';
+import Register from './pages/auth/Register';
+import Login from './pages/auth/Login';
+import Recipe from './pages/recipes/Recipe';
+import Recipes from './pages/recipes/Recipes';
 
 function App() {
 	return (
 		<>
 			<BrowserRouter basename='/'>
 				<Routes>
-          <Route path='/' element={<Home />}/>
-        </Routes>
+					<Route
+						path='/'
+						element={<Home />}
+					/>
+					<Route
+						path='/recipes'
+						element={<Recipes />}
+					/>
+					<Route
+						path='/recipe/:id'
+						element={<Recipe />}
+					/>
+					<Route
+						path='/login'
+						element={<Login />}
+					/>
+					<Route
+						path='/register'
+						element={<Register />}
+					/>
+				</Routes>
 			</BrowserRouter>
 		</>
 	);
