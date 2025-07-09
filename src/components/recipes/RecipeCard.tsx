9b07f4032313d450cc21b0e2bb8f09dec419cfd6
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-09 19:47:31 -0300

    feat: create recipe card

---
+++ src/components/recipes/RecipeCard.tsx
@@ -0,0 +1,67 @@
+import { Clock, Heart, MessageCircleIcon, Users2 } from 'lucide-react';
+import pizza from '../../assets/pizza.jpg';
+import pfp from '../../assets/anime.png';
+
+export default function RecipeCard() {
+	return (
+		<>
+			<div className='w-full h-full overflow-hidden transition duration-150 ease-in bg-white border-white shadow cursor-pointer group relateive lg:h-full hover:outline outline-white rounded-xl hover:outline-emerald-200 hover:shadow-lg shadow-gray-600/10'>
+				<div className='relative rounded-xl'>
+					<div className='absolute flex justify-between w-full px-2 top-4 z-1'>
+						<div className='px-4 py-0.5 text-red-700 bg-red-300/90 font-semibold text-sm rounded-xl'>Dificil</div>
+						<div className='px-4 py-0.5 text-gray-800 bg-gray-100/90 font-semibold text-sm rounded-xl'>Prato principal</div>
+					</div>
+					<img
+						src={pizza}
+						alt=''
+						className='object-cover w-full transition-transform duration-150 h-60 rounded-t-2xl group-hover:scale-105'
+					/>
+				</div>
+				<div className='px-6 py-4 bg-white border border-gray-300 shadow '>
+					<div>
+						<h4 className='text-2xl font-semibold truncate transition duration-150 ease-in group-hover:text-emerald-600'>sushi caseiro - temaki de salmao</h4>
+						<p className='text-gray-600 line-clamp-2'>
+							Adasdlpasdlp aslpdlpa sdlpsd lpasdplasd plasdlpprenda a fazer temaki de salmão em casa! Fresco, saboroso e mais fácil do que parece.
+						</p>
+					</div>
+					<div className='flex justify-between py-4'>
+						<div className='flex items-center gap-1 text-gray-500'>
+							<Clock size={16} />
+							<span className='text-sm'>90 min</span>
+						</div>
+						<div className='flex items-center gap-1 text-gray-500'>
+							<Users2 size={16} />
+							<span className='text-sm'>4 porções</span>
+						</div>
+					</div>
+					<div className='border-t border-gray-400/30'>
+						<div className='flex items-center justify-between'>
+							<div className='flex pt-4'>
+								<div>
+									<img
+										src={pfp}
+										alt='pfp'
+										className='mr-2 text-xs rounded-full h-7 w-7 outline outline-black/30'
+									/>
+								</div>
+								<div className='flex flex-col'>
+									<span className='text-sm font-semibold text-gray-700'>admin</span>
+								</div>
+							</div>
+							<div className='flex gap-4'>
+								<div className='flex items-center gap-1 text-gray-600'>
+                                    <Heart size={16} />
+                                    <span className='text-sm'>0</span>
+                                </div>
+								<div className='flex items-center gap-1 text-gray-600'>
+                                    <MessageCircleIcon size={16}/>
+                                    <span className='text-sm'>0</span>
+                                </div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
