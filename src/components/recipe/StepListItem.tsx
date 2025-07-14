a81c9ece6b9545ec3598677b9fdda8212d9f6e3e
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-14 16:22:23 -0300

    chore: change import names

--- src/components/recipe/StepListItem.tsx
+++ src/components/recipe/StepListItem.tsx
@@ -1,6 +1,6 @@
-import type { StepListItemProps } from "../../types/recipe";
+import type { stepListItemProps } from "../../types/recipe";
 
-export default function StepListItem({step, instructions}: StepListItemProps) {
+export default function StepListItem({step, instructions}: stepListItemProps) {
 	return (
 		<li className='flex'>
 			<div className='flex items-center justify-center flex-shrink-0 w-8 h-8 text-sm font-semibold text-white rounded-full bg-emerald-600'>{step}</div>
