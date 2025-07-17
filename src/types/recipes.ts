9b976cb1ddc926073ea26e0bd12dde9d41eaad60
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-17 18:00:58 -0300

    feat: create recipe types

---
+++ src/types/recipes.ts
@@ -0,0 +1,39 @@
+export type recipeForm = {
+	image: string;
+	title: string;
+	description: string;
+	category: string;
+	difficulty: string;
+	time: number;
+	portions: number;
+	ingredients: {
+		name: string;
+		quantity: string;
+		unit: string;
+	}[];
+	instructions: {
+		step: number;
+		description: string;
+	}[];
+};
+
+export type recipes = {
+    _id: string
+	image: string;
+	title: string;
+	description: string;
+	category: string;
+	difficulty: string;
+	cookingTime: number;
+	portions: number;
+	ingredients: {
+		name: string;
+		quantity: string;
+		unit: string;
+	}[];
+	instructions: {
+		step: number;
+		description: string;
+	}[];
+    likesCount: number
+};
