eea516777f2389930719c34da80e2db8d18a79a0
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-21 11:55:05 -0300

    feat: add RecipeCard props types

--- src/types/components/recipe.ts
+++ src/types/components/recipe.ts
@@ -1,7 +1,18 @@
+export type RecipeCardProps = {
+	id: string;
+	difficulty: 'Facil' | 'Medio' | 'Dificil';
+	category: string;
+	image: string;
+	description: string;
+	title: string;
+	cookTime: number;
+	portions: number;
+};
+
 export type stepListItemProps = {
-    step: number
-    instructions: string
-}
+	step: number;
+	instructions: string;
+};
 
 export type ingredientListProps = {
 	unity: string;
