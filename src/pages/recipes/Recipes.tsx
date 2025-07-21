136bf0034bca66fdda4224db037bd2dcd289522c
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-21 11:54:09 -0300

    feat: added loader, also implicitlyset difficulty types

--- src/pages/recipes/Recipes.tsx
+++ src/pages/recipes/Recipes.tsx
@@ -14,12 +14,15 @@ const API_URL = import.meta.env.VITE_API_URL;
 export default function Recipes() {
 	const [sidebarOpen, setSidebarOpen] = useState(false);
 	const [recipes, setRecipes] = useState<recipes[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
 	const pageIndex = 1;
 
 	async function fetchRecipes() {
+		setIsLoading(true);
 		try {
 			const response = await axios.get(`${API_URL}/api/v1/recipes`);
 			setRecipes(response.data.data);
+			setIsLoading(false);
 		} catch (error) {
 			console.log(error);
 		}
@@ -44,102 +47,113 @@ export default function Recipes() {
 				/>
 				<Header toggleSide={handleSideBarToggle} />
 			</header>
-			<main className='container max-w-[1400px] mx-auto px-4 relative top-18'>
-				<section className='pb-8'>
-					<div className='flex items-center gap-1 py-4'>
-						<div className='relative p-2 text-white bg-emerald-600 bottom-1 rounded-xl'>
-							<TrendingUpIcon size={30} />
-						</div>
-						<div className='flex flex-col px-2'>
-							<h1 className='text-2xl font-bold'>Receitas Populares</h1>
-							<p className='font-semibold text-gray-600'>As mais curtidas!</p>
-						</div>
+			{isLoading ? (
+				<div className='flex items-center justify-center mt-80'>
+					<div className='lds-ring text-emerald-600'>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
 					</div>
-					<div className='flex flex-col grid-cols-2 gap-4 lg:grid'>
-						<div className='hidden row-span-2 lg:block h-85'>
-							<LargeFeaturedRecipe />
-						</div>
-						<SmallFeaturedRecipeCard />
-						<SmallFeaturedRecipeCard />
-					</div>
-				</section>
-				<section className='pb-4'>
-					<div className='p-4 bg-white border border-gray-300 shadow-xs rounded-xl'>
-						<div className='flex items-center gap-4'>
-							<TrendingUpIcon
-								className='text-emerald-600'
-								size={22}
-							/>
-							<span className='text-lg font-semibold'>Explorar receitas</span>
-						</div>
-						<div className='flex flex-col gap-4 pt-4 md:flex-row'>
-							<div className='relative w-full'>
-								<input
-									type='text'
-									name='search'
-									id='search'
-									className='w-full p-4 pl-12 bg-gray-100 border border-gray-300 rounded-2xl outline-0 focus:ring focus:ring-emerald-600/60 '
-								/>
-								<Search className='absolute text-gray-500 top-4 left-4' />
+				</div>
+			) : (
+				<main className='container max-w-[1400px] mx-auto px-4 relative top-18'>
+					<section className='pb-8'>
+						<div className='flex items-center gap-1 py-4'>
+							<div className='relative p-2 text-white bg-emerald-600 bottom-1 rounded-xl'>
+								<TrendingUpIcon size={30} />
 							</div>
-							<div className='flex flex-col gap-4'>
-								<div className='flex items-center gap-4'>
-									<label
-										htmlFor='filter'
-										className='hidden sm:block'>
-										<Filter />
-									</label>
-									<select
-										className='w-full px-6 py-4 border border-gray-300 md:w-auto rounded-2xl'
-										name='filter'
-										id='filter'>
-										<option defaultChecked>Todas</option>
-										<option value=''>Café da manha</option>
-										<option value=''>Almoço</option>
-										<option value=''>Jantar</option>
-										<option value=''>Entrada</option>
-										<option value=''>Prato Principal</option>
-										<option value=''>Sobremesa</option>
-										<option value=''>Bebida</option>
-										<option value=''>Lanche</option>
-									</select>
-									<div>
-										<button className='w-full px-8 py-4 text-white cursor-pointer md:w-auto bg-emerald-600 rounded-2xl hover:bg-emerald-700'>Buscar</button>
+							<div className='flex flex-col px-2'>
+								<h1 className='text-2xl font-bold'>Receitas Populares</h1>
+								<p className='font-semibold text-gray-600'>As mais curtidas!</p>
+							</div>
+						</div>
+						<div className='flex flex-col grid-cols-2 gap-4 lg:grid'>
+							<div className='hidden row-span-2 lg:block h-85'>
+								<LargeFeaturedRecipe />
+							</div>
+							<SmallFeaturedRecipeCard />
+							<SmallFeaturedRecipeCard />
+						</div>
+					</section>
+					<section className='pb-4'>
+						<div className='p-4 bg-white border border-gray-300 shadow-xs rounded-xl'>
+							<div className='flex items-center gap-4'>
+								<TrendingUpIcon
+									className='text-emerald-600'
+									size={22}
+								/>
+								<span className='text-lg font-semibold'>Explorar receitas</span>
+							</div>
+							<div className='flex flex-col gap-4 pt-4 md:flex-row'>
+								<div className='relative w-full'>
+									<input
+										type='text'
+										name='search'
+										id='search'
+										className='w-full p-4 pl-12 bg-gray-100 border border-gray-300 rounded-2xl outline-0 focus:ring focus:ring-emerald-600/60 '
+									/>
+									<Search className='absolute text-gray-500 top-4 left-4' />
+								</div>
+								<div className='flex flex-col gap-4'>
+									<div className='flex items-center gap-4'>
+										<label
+											htmlFor='filter'
+											className='hidden sm:block'>
+											<Filter />
+										</label>
+										<select
+											className='w-full px-6 py-4 border border-gray-300 md:w-auto rounded-2xl'
+											name='filter'
+											id='filter'>
+											<option defaultChecked>Todas</option>
+											<option value=''>Café da manha</option>
+											<option value=''>Almoço</option>
+											<option value=''>Jantar</option>
+											<option value=''>Entrada</option>
+											<option value=''>Prato Principal</option>
+											<option value=''>Sobremesa</option>
+											<option value=''>Bebida</option>
+											<option value=''>Lanche</option>
+										</select>
+										<div>
+											<button className='w-full px-8 py-4 text-white cursor-pointer md:w-auto bg-emerald-600 rounded-2xl hover:bg-emerald-700'>Buscar</button>
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				</section>
-				<section>
-					<div className='grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-						{recipes.map((recipes) => (
-							<RecipeCard
-								key={recipes._id}
-								id={recipes._id}
-								category={recipes.category}
-								cookTime={recipes.cookingTime}
-								description={recipes.description}
-								difficulty={recipes.difficulty}
-								image={recipes.image}
-								portions={recipes.portions}
-								title={recipes.title}
-							/>
-						))}
-					</div>
-					<div className='flex items-center justify-center py-8'>
-						<div className='flex gap-4'>
-							<div className='p-1 text-white cursor-pointer bg-emerald-600 rounded-xl hover:bg-emerald-700'>
-								<ArrowLeft size={30} />
-							</div>
-							<div className='text-2xl font-semibold'>{pageIndex}</div>
-							<div className='p-1 text-white cursor-pointer bg-emerald-600 rounded-xl hover:bg-emerald-700'>
-								<ArrowRight size={30} />
+					</section>
+					<section>
+						<div className='grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+							{recipes.map((recipes) => (
+								<RecipeCard
+									key={recipes._id}
+									id={recipes._id}
+									category={recipes.category}
+									cookTime={recipes.cookingTime}
+									description={recipes.description}
+									difficulty={recipes.difficulty}
+									image={recipes.image}
+									portions={recipes.portions}
+									title={recipes.title}
+								/>
+							))}
+						</div>
+						<div className='flex items-center justify-center py-8'>
+							<div className='flex gap-4'>
+								<div className='p-1 text-white cursor-pointer bg-emerald-600 rounded-xl hover:bg-emerald-700'>
+									<ArrowLeft size={30} />
+								</div>
+								<div className='text-2xl font-semibold'>{pageIndex}</div>
+								<div className='p-1 text-white cursor-pointer bg-emerald-600 rounded-xl hover:bg-emerald-700'>
+									<ArrowRight size={30} />
+								</div>
 							</div>
 						</div>
-					</div>
-				</section>
-			</main>
+					</section>
+				</main>
+			)}
 		</>
 	);
 }
