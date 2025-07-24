7afc71b157eec1c50a3b1cf6a90020ed5aeb59f7
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-24 17:04:29 -0300

    feat: wrap app with auth & user providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 import './styles.css';
-
+import { AuthProvider } from './provider/auth.tsx';
+import { UserProvider } from './provider/user.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<AuthProvider>
+			<UserProvider>
+				<App />
+			</UserProvider>
+		</AuthProvider>
+	</StrictMode>,
+);
