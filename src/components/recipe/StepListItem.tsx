85c8f6c643778a6338d4eab15e93483e9c75bc15
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-25 15:29:44 -0300

    style: centered container & removed some spacing

--- src/components/recipe/StepListItem.tsx
+++ src/components/recipe/StepListItem.tsx
@@ -2,9 +2,9 @@ import type { stepListItemProps } from "../../types/components/recipesComponents
 
 export default function StepListItem({step, instructions}: stepListItemProps) {
 	return (
-		<li className='flex'>
+		<li className='flex items-center'>
 			<div className='flex items-center justify-center flex-shrink-0 w-8 h-8 text-sm font-semibold text-white rounded-full bg-emerald-600'>{step}</div>
-			<p className='ml-4 text-sm text-gray-900'>{instructions}</p>
+			<p className='ml-3 text-sm text-gray-900'>{instructions}</p>
 		</li>
 	);
 }
