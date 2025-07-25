a833500050f58ebf73050c4e025ea2a748db3316
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-25 13:43:21 -0300

    feat: add getRecipe & createRecipe functions

--- src/services/recipes.ts
+++ src/services/recipes.ts
@@ -1,3 +1,4 @@
+import type { recipeForm } from '../types/recipes';
 import api from './api';
 
 export async function getFeaturedRecipes() {
@@ -13,6 +14,26 @@ export async function getFeaturedRecipes() {
 export async function getAllRecipes() {
 	try {
 		const response = await api.get(`/api/v1/recipes`);
+		return response.data.data;
+	} catch (error) {
+		console.log('ERRO NA REQUISIÇÃO, POR FAVOR TENTE NOVAMENTE DEPOIS ', error);
+		throw error;
+	}
+}
+
+export async function getRecipe(recipeID: string) {
+	try {
+		const response = await api.get(`/api/v1/recipes/${recipeID}`);
+		return response.data.data;
+	} catch (error) {
+		console.log('ERRO NA REQUISIÇÃO, POR FAVOR TENTE NOVAMENTE DEPOIS ', error);
+		throw error;
+	}
+}
+
+export async function createRecipe(data: recipeForm) {
+	try {
+		const response = await api.post(`/api/v1/recipes/`, data);
 		return response.data.data;
 	} catch (error) {
 		console.log('ERRO NA REQUISIÇÃO, POR FAVOR TENTE NOVAMENTE DEPOIS ', error);
