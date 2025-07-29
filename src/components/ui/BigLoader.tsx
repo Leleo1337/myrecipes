a4ccd02d57eab7524130b4b61c85a9e87cf9edf4
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-07-29 12:37:22 -0300

    styles: centered loader

--- src/components/ui/BigLoader.tsx
+++ src/components/ui/BigLoader.tsx
@@ -1,7 +1,7 @@
 export default function BigLoader({color}: {color: 'white' | 'emerald'}) {
 	return (
 		<>
-			<div className='absolute inset-0 flex items-center justify-center'>
+			<div className='absolute top-1/2 left-1/2 -translate-1/2 flex items-center justify-center'>
 				<div className={`lds-ring ${color === 'white' ? 'text-white' : 'text-emerald-600'}`}>
 					<div></div>
 					<div></div>
