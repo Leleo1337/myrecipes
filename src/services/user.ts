b2fb6c9c66d80a81208d0adeefd6968ce4a99c01
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-08-04 17:58:59 -0300

    feat: add new function to fetch user by id, also rename old function getUserData to getLoggedInUserData

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,9 +1,19 @@
 import api from './api';
 
-export async function getUserData() {
+export async function getLoggedInUserData() {
 	try {
 		const userData = await api.get(`/api/v1/user/me`);
-        return userData.data.user
+		return userData.data.user;
+	} catch (err) {
+		console.log('[ERRO] ALGO DEU ERRADO: ', err);
+		throw err;
+	}
+}
+
+export async function getUserData(userID: string | undefined) {
+	try {
+		const userData = await api.get(`/api/v1/user/${userID}`);
+		return userData.data.user;
 	} catch (err) {
 		console.log('[ERRO] ALGO DEU ERRADO: ', err);
 		throw err;
