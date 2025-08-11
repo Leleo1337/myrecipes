8f69fddef0ba90d3875e15d7a9f72907857fb57b
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-08-11 16:25:35 -0300

    chore: create home page types folder

---
+++ src/types/components/homeComponentsProps.ts
@@ -0,0 +1,13 @@
+export type infoCardProps = {
+	icon: any;
+	stats: string;
+	paragraph: string;
+	textColor: 'yellow' | 'red' | 'blue';
+};
+
+export type featuredCardProps = {
+	icon: any;
+	title: string;
+	text: string;
+	color: 'darkgreen' | 'green' | 'orange';
+};
