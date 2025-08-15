90d070d6318873568a5c6f2f380ebd95d3137986
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-08-15 16:50:47 -0300

    feat: add size prop to control buttons sizes

--- src/components/ui/PaginationButtons.tsx
+++ src/components/ui/PaginationButtons.tsx
@@ -3,42 +3,43 @@ import { ArrowLeft, ArrowRight } from 'lucide-react';
 export type paginationButtonsProps = {
 	pageIndex: number;
 	pageLimit: number;
+	size: 'small' | 'medium';
 	prevPage: () => void;
 	nextPage: () => void;
 };
 
-export default function PaginationButtons({ pageIndex, pageLimit, prevPage, nextPage }: paginationButtonsProps) {
+export default function PaginationButtons({ pageIndex, pageLimit, size, prevPage, nextPage }: paginationButtonsProps) {
 	return (
 		<>
 			<div className='flex items-center justify-center gap-4 py-6'>
 				{pageIndex === 1 ? (
 					<button disabled>
 						<ArrowLeft
-							size={32}
+							size={size === 'small' ? 24 : 32}
 							className='text-gray-600 transition duration-100 ease-in cursor-pointer hover:text-gray-500'
 						/>
 					</button>
 				) : (
 					<button>
 						<ArrowLeft
-							size={32}
+							size={size === 'small' ? 24 : 32}
 							className={`transition duration-100 ease-in cursor-pointer hover:text-emerald-600`}
 							onClick={prevPage}
 						/>
 					</button>
 				)}
-				<p className='mb-1 text-2xl font-semibold'>{pageIndex}</p>
+				<p className={`mb-1 ${size === 'small' ? 'text-xl' : 'text-2xl'} font-semibold`}>{pageIndex}</p>
 				{pageIndex === pageLimit ? (
 					<button disabled>
 						<ArrowRight
-							size={32}
+							size={size === 'small' ? 24 : 32}
 							className='text-gray-600 transition duration-100 ease-in cursor-pointer hover:text-gray-500'
 						/>
 					</button>
 				) : (
 					<button>
 						<ArrowRight
-							size={32}
+							size={size === 'small' ? 24 : 32}
 							className={`transition duration-100 ease-in cursor-pointer hover:text-emerald-600`}
 							onClick={nextPage}
 						/>
