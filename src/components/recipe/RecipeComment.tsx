b8019ddd397055a8f3804cfe527baa062de5c276
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-08-15 17:10:59 -0300

    feat: add real date

--- src/components/recipe/RecipeComment.tsx
+++ src/components/recipe/RecipeComment.tsx
@@ -2,8 +2,17 @@ import Avatar from 'react-avatar';
 import type { commentProps } from '../../types/components/comments';
 
 export default function RecipeComment({ createdBy, text, createdAt }: commentProps) {
+	const date = new Date(createdAt);
+	const formattedDate = date.toLocaleDateString('pt-BR', {
+		day: '2-digit',
+		month: '2-digit',
+		year: 'numeric',
+		hour: '2-digit',
+		minute: '2-digit',
+	});
+
 	return (
-		<div className='flex items-center p-4 bg-slate-50 rounded-2xl'>
+		<div className='flex p-4 bg-slate-50 rounded-2xl'>
 			{createdBy.profilePicture ? (
 				<img
 					src={createdBy.profilePicture}
@@ -20,9 +29,9 @@ export default function RecipeComment({ createdBy, text, createdAt }: commentPro
 			<div className='flex flex-col ml-2'>
 				<div className='flex items-center'>
 					<h6 className='font-semibold'>{createdBy.name ? createdBy.name : 'username'}</h6>
-					<span className='flex items-center ml-2 text-xs font-semibold text-gray-500'>{createdAt}</span>
+					<span className='flex items-center ml-2 text-xs font-semibold text-gray-500'>{formattedDate}</span>
 				</div>
-				<p className='text-sm text-gray-600'>{text}</p>
+				<p className='text-sm text-gray-600 break-all'>{text}</p>
 			</div>
 		</div>
 	);
