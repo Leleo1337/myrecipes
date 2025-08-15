df471a8d671c98271d920026ee9f5037c97c0cf8
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-08-15 16:50:24 -0300

    chore: change props names

--- src/components/recipe/RecipeComment.tsx
+++ src/components/recipe/RecipeComment.tsx
@@ -1,26 +1,26 @@
 import Avatar from 'react-avatar';
 import type { commentProps } from '../../types/components/comments';
 
-export default function RecipeComment({ profilePicture, username, text, date }: commentProps) {
+export default function RecipeComment({ createdBy, text, createdAt }: commentProps) {
 	return (
 		<div className='flex items-center p-4 bg-slate-50 rounded-2xl'>
-			{profilePicture ? (
+			{createdBy.profilePicture ? (
 				<img
-					src={profilePicture}
+					src={createdBy.profilePicture}
 					alt='user pfp'
 					className='w-8 h-8 mr-2 text-xs rounded-full outline-1 outline-black/20'
 				/>
 			) : (
 				<Avatar
-					name={username}
+					name={createdBy.name}
 					size='32'
 					className='w-8 h-8 text-xs rounded-full'
 				/>
 			)}
 			<div className='flex flex-col ml-2'>
 				<div className='flex items-center'>
-					<h6 className='font-semibold'>{username ? username : 'username'}</h6>
-					<span className='flex items-center ml-2 text-xs font-semibold text-gray-500'>{date? date: '29-4-2014'}</span>
+					<h6 className='font-semibold'>{createdBy.name ? createdBy.name : 'username'}</h6>
+					<span className='flex items-center ml-2 text-xs font-semibold text-gray-500'>{createdAt}</span>
 				</div>
 				<p className='text-sm text-gray-600'>{text}</p>
 			</div>
