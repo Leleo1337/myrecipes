423a71f7bf0ac6dc7a8f634761c7c39cf0efee81
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-08-25 17:29:15 -0300

    feat: create JOI error translator util function

---
+++ src/utils/translateJoiError.ts
@@ -0,0 +1,9 @@
+export function translateJoiError(type: string, field: string, limit?: number) {
+	const messages: Record<string, string> = {
+		'string.empty': `O campo ${field} é obrigatório.`,
+		'string.email': `O campo ${field} deve ser um e-mail válido.`,
+		'string.min': `O campo ${field} deve ter ao menos ${limit} caracteres`
+	};
+
+	return messages[type] || 'Erro desconhecido';
+}
