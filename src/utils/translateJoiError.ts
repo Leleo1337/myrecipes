d3c1638f90996a90c1233df11dfbe9f52d572eec
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-08-29 18:40:33 -0300

    feat: added string.uri error

--- src/utils/translateJoiError.ts
+++ src/utils/translateJoiError.ts
@@ -2,7 +2,8 @@ export function translateJoiError(type: string, field: string, limit?: number) {
 	const messages: Record<string, string> = {
 		'string.empty': `O campo ${field} é obrigatório.`,
 		'string.email': `O campo ${field} deve ser um e-mail válido.`,
-		'string.min': `O campo ${field} deve ter ao menos ${limit} caracteres`
+		'string.min': `O campo ${field} deve ter ao menos ${limit} caracteres`,
+		'string.uri': `O campo ${field} deve ser um link valido`,
 	};
 
 	return messages[type] || 'Erro desconhecido';
