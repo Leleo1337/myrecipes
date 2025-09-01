aecb78daf5c4f787fe8375128f026e333216278b
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-09-01 17:26:54 -0300

    feat: add deleteComment & updateComment fetchers

--- src/services/comments.ts
+++ src/services/comments.ts
@@ -9,9 +9,27 @@ export async function fetchRecipeComments(recipeID: string, page: number) {
 	}
 }
 
+export async function updateComment(recipeID: string, commentID: string, commentData: any) {
+	try {
+		const response = await api.patch(`/api/v1/recipes/${recipeID}/comments/${commentID}`, commentData);
+		return response.data.data;
+	} catch (err) {
+		throw err;
+	}
+}
+
 export async function createComment(commentData: any, recipeID: string | undefined) {
 	try {
 		const response = await api.post(`/api/v1/recipes/${recipeID}/comments`, commentData);
+		return response.data.data;
+	} catch (err) {
+		throw err;
+	}
+}
+
+export async function deleteComent(recipeID: string, commentID: string) {
+	try {
+		const response = await api.delete(`/api/v1/recipes/${recipeID}/comments/${commentID}`);
 		return response.data.data;
 	} catch (err) {
 		throw err;
